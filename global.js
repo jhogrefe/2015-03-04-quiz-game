34ec800a9da303747fc136e29dbdf3fdcc327563
Jason Hogrefe <hogrefejason@gmail.com>
2015-03-04 13:44:55 -0600

    Made questions in js file.

---
+++ global.js
@@ -0,0 +1,29 @@
+question_one = {
+  question: "What is 2 + 2?",
+  choices: [3, 4, 7, 9],
+  answer: 4
+};
+
+question_two = {
+  question: "What is 3 + 2?",
+  choices: [8, 4, 5, 2],
+  answer: 5
+};
+
+question_three = {
+  question: "What is 6 + 7?",
+  choices: [33, 14, 13, 9],
+  answer: 13
+};
+
+question_four = {
+  question: "What is 5 - 2?",
+  choices: [3, 8, 0, 11],
+  answer: 3
+};
+
+question_five = {
+  question: "What is 5 + 5?",
+  choices: [23, 1, 10, 11],
+  answer: 10
+};
